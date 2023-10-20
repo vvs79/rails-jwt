a05cb514bf3f3938a7a1f81265cfdf83c4ce2bf3
vvs79 <vidoc1979@gmail.com>
2023-10-20 11:25:35 +0300

    rails-jwt

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class AuthenticationController < ApplicationController
+  before_action :authorize_request, except: :login
+
+  # POST /auth/login
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user&.authenticate(params[:password])
+      token = jwt_encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                     name: @user.name }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def login_params
+    params.permit(:email, :password)
+  end
+end
